11f95f5db3b2234778e33f4bec9c8b088062d579
nibin-matteo-git <nibinmatteo5@gmail.com>
2024-12-06 08:39:39 +0000

    day 5 not working

--- 2024/day5/main.go
+++ 2024/day5/main.go
@@ -14,12 +14,11 @@ func main() {
 	cwd, _ := os.Getwd()
 	inputPath := filepath.Join(cwd, "input.txt")
 	printOrder, printRules := GetInput(inputPath)
-	printOrderList1, printOrderList2 := make([]int,0), make([]int, 0)
+	printOrderList1, printOrderList2 := make([]int, 0), make([]int, 0)
 
 	finalOut := 0
 
-
-	MapFuncToList(printRules, func(input string)int{
+	MapFuncToList(printRules, func(input string) int {
 		int1, _ := strconv.Atoi(strings.Split(input, "|")[0])
 		int2, _ := strconv.Atoi(strings.Split(input, "|")[1])
 		printOrderList1 = append(printOrderList1, int1)
@@ -27,19 +26,19 @@ func main() {
 		return 0
 	})
 	myRules := addToMapTwoLists(printOrderList1, printOrderList2)
-	parsedPrintInput := MapFuncToList(printOrder, func(input string) []int{
+	parsedPrintInput := MapFuncToList(printOrder, func(input string) []int {
 		out := make([]int, 0)
-		for _, num := range(strings.Split(input, ",")){
+		for _, num := range strings.Split(input, ",") {
 			intNum, _ := strconv.Atoi(num)
 			out = append(out, intNum)
 		}
 		return out
 	})
-	for _, printInput := range(parsedPrintInput){
+	for _, printInput := range parsedPrintInput {
 		// _, middleIndex := checkPrintOrder(myRules,printInput)
 		// finalOut+=middleIndex
 		correctOrder, _ := checkPrintOrder(myRules, printInput)
-		if !correctOrder{
+		if !correctOrder {
 			finalOut += calcMiddleIndexVal(fixPrintingOrder(myRules, printInput))
 		}
 	}
@@ -47,43 +46,42 @@ func main() {
 
 }
 
-func calcMiddleIndexVal(input []int)int{
-	return input[int(math.Round(float64(len(input))/2))-1] 
+func calcMiddleIndexVal(input []int) int {
+	return input[int(math.Round(float64(len(input))/2))-1]
 }
 
-
-func fixPrintingOrder(rules map[int][]int, input[]int)[]int{
-	earliestPage:=-1
+func fixPrintingOrder(rules map[int][]int, input []int) []int {
+	earliestPage := -1
 	alrPrintedPgs := make(map[int]int, 0)
-	for i, pg := range(input){
-		for _, rule := range(rules[pg]){
+	for i, pg := range input {
+		for _, rule := range rules[pg] {
 			earliestPageIndex, wrongOrder := alrPrintedPgs[rule]
-			if wrongOrder{
-				if earliestPage==-1 || earliestPage>earliestPageIndex{
-					earliestPage=earliestPageIndex
+			if wrongOrder {
+				if earliestPage == -1 || earliestPage > earliestPageIndex {
+					earliestPage = earliestPageIndex
 				}
 			}
 		}
-		if earliestPage!=-1{
-			newInput := append(input[:i],input[i+1:]...)
+		if earliestPage != -1 {
+			newInput := append(input[:i], input[i+1:]...)
 			newInput = slices.Insert(newInput, earliestPage, pg)
 			return fixPrintingOrder(rules, newInput)
 		}
-		alrPrintedPgs[pg]=i
+		alrPrintedPgs[pg] = i
 	}
 	return input
 }
 
-func checkPrintOrder(rules map[int][]int, input []int)(bool, int){
+func checkPrintOrder(rules map[int][]int, input []int) (bool, int) {
 	alrPrintedPgs := make(map[int]int, 0)
-	for _, pg := range(input){
-		for _, rule := range(rules[pg]){
+	for _, pg := range input {
+		for _, rule := range rules[pg] {
 			_, wrongOrder := alrPrintedPgs[rule]
-			if wrongOrder{
-				return false, 0 
+			if wrongOrder {
+				return false, 0
 			}
 		}
-		alrPrintedPgs[pg]+=1
+		alrPrintedPgs[pg] += 1
 	}
-	return true, input[int(math.Round(float64(len(input))/2))-1] 
+	return true, input[int(math.Round(float64(len(input))/2))-1]
 }
