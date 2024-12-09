2fc13dd71f7a454a42c55043deb664229146c217
nibin-matteo-git <nibinmatteo5@gmail.com>
2024-12-09 12:47:51 +0000

    Day 9 p2 finished

--- 2024/day9/main.go
+++ 2024/day9/main.go
@@ -15,8 +15,52 @@ func main() {
 	input := GetInput(inputPath)
 	expandedInput := generateExpandedView(input)
 	// fmt.Println(expandedInput)
-	// fmt.Println(p1(expandedInput))
-	fmt.Println(caclCheckSum(p1(expandedInput)))
+	fmt.Println(caclCheckSum(p2(input, expandedInput)))
+	// fmt.Println(caclCheckSum(p1(expandedInput)))
+}
+
+func p2(rawInput string, inputt []string)[]string{
+	input := make([]string, len(inputt))
+	copy(input, inputt)
+	emptLocs := p2GetEmptLocs(rawInput)
+	for i:=len(input)-1; i>=0; i--{
+		if input[i]!="."{
+			// fmt.Println(input)
+			fileIndex, _ := strconv.Atoi(input[i])
+			lenOfFile, _ := strconv.Atoi(string(rawInput[fileIndex*2]))
+			for j:=0; j<len(emptLocs); j++{
+				if emptLocs[j][1]>=int(lenOfFile) && emptLocs[j][0]<i{
+					for k:=0; k<int(lenOfFile); k++{
+						input[emptLocs[j][0]]=input[i]
+						input[i]="."
+						emptLocs[j][1]-=1
+						emptLocs[j][0]+=1
+						i-=1
+					}
+					i++
+					break
+				}
+			}
+		}
+	}
+	return input
+}
+
+
+// output of this func is [[<start index of empty space>, length of free space]]
+func p2GetEmptLocs(rawInput string)[][]int{
+	emptLocs := make([][]int, 0)
+	expandedInputIndex := 0
+	for i, val := range(rawInput){
+		indInc, _ := strconv.Atoi(string(val))
+		if i%2==0{
+			expandedInputIndex+=indInc
+		}else{
+			emptLocs = append(emptLocs, []int{expandedInputIndex, indInc})
+			expandedInputIndex+=indInc
+		}
+	}
+	return emptLocs
 }
 
 func p1(input []string)[]string{
